d9d31bebd2daec8e8fad5cd51940208fd588e74b
uthree <uthree271828@gmail.com>
2022-11-20 17:27:47 +0900

    fix

--- train_vae_first.py
+++ train_vae_first.py
@@ -1,24 +1,37 @@
 from dataset import ImageDataset
-from vae import VAE, Encoder, Decoder
+from vae import VAE, Encoder, Decoder, Discriminator
+import torch.nn.functional as F
+import torchvision.transforms as T
 import sys
 import os
 from tqdm import tqdm
 import torch
 import torch.optim as optim
+from PIL import Image
+import numpy as np
 
 encoder_path = "./vae_encoder.pt"
 decoder_path = "./vae_decoder.pt"
-batch_size = 1
+discriminator_path = "./vae_discriminator.pt"
+result_dir = "./vae_result/"
+batch_size = 4
 num_epoch = 3000
 learning_rate = 1e-4
 image_size = 512
+crop_size = (192, 192)
+num_crop_per_batch = 2
 max_dataset_size = 10000
-weight_kl = 0.2
+weight_kl = 1.0
+weight_adv = 1.0
+weight_recon = 5.0
+weight_feat = 1.0
 use_autocast = True
 
 ds = ImageDataset(sys.argv[1:], max_len=max_dataset_size, size=image_size)
 encoder = Encoder()
 decoder = Decoder()
+discriminator = Discriminator()
+crop = T.RandomCrop(crop_size)
 
 if os.path.exists(encoder_path):
     encoder.load_state_dict(torch.load(encoder_path))
@@ -28,11 +41,20 @@ if os.path.exists(decoder_path):
     decoder.load_state_dict(torch.load(decoder_path))
     print("Decoder Model Loaded.")
 
+if os.path.exists(discriminator_path):
+    discriminator.load_state_dict(torch.load(discriminator_path))
+    print("Discriminator Model Loaded.")
+
+if not os.path.exists(result_dir):
+    os.mkdir(result_dir)
+
 vae = VAE(encoder, decoder)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"device: {device}")
 vae.to(device)
-optimizer = optim.RAdam(vae.parameters(), lr=learning_rate)
+discriminator.to(device)
+optimizer_vae = optim.RAdam(vae.parameters(), lr=learning_rate)
+optimizer_d = optim.RAdam(discriminator.parameters(), lr=learning_rate)
 scaler = torch.cuda.amp.GradScaler(enabled=use_autocast)
 
 dl = torch.utils.data.DataLoader(ds, batch_size=batch_size, shuffle=True)
@@ -41,18 +63,50 @@ for epoch in range(num_epoch):
     print(f"Epoch #{epoch}")
     for batch, image in enumerate(dl):
         N = image.shape[0]
-        optimizer.zero_grad()
-        image = image.to(device)
         
-        with torch.cuda.amp.autocast(enabled=use_autocast):
-            recon_loss, kl_loss = vae.calclate_loss(image)
-            loss = recon_loss + kl_loss * weight_kl
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-    
-        scaler.update()
-        bar.set_description(desc=f"Recon: {recon_loss.item():.4f}, KL {kl_loss.item():.4f}")
+        for i in range(num_crop_per_batch):
+            # Train G.
+            optimizer_vae.zero_grad()
+            image = image.to(device)
+            image = crop(image)
+        
+            with torch.cuda.amp.autocast(enabled=use_autocast):
+                recon_loss, kl_loss, y = vae.calclate_loss(image)
+                logit, feat_loss = discriminator.calclate_logit_and_feature_matching(y, image)
+                adv_loss = F.relu(-logit).mean()
+                loss = recon_loss * weight_recon + kl_loss * weight_kl + adv_loss * weight_adv + feat_loss * weight_feat
+            scaler.scale(loss).backward()
+            scaler.step(optimizer_vae)
+            fake = y.detach()
+
+            # Train D.
+            optimizer_d.zero_grad()
+            with torch.cuda.amp.autocast(enabled=use_autocast):
+                fake_logit = discriminator.calclate_logit(fake)
+                real_logit = discriminator.calclate_logit(image)
+                loss_d = F.relu(0.5-real_logit).mean() + F.relu(0.5+fake_logit).mean()
+            scaler.scale(loss_d).backward()
+            scaler.step(optimizer_d)
+
+            scaler.update()
+            bar.set_description(desc=f"Recon: {recon_loss.item():.4f}, KL {kl_loss.item():.4f}, Adv: {adv_loss.item():.4f}, Feat: {feat_loss.item():.4f}, Disc: {loss_d.item():.4f}")
+            bar.update(0)
         bar.update(N)
-        if batch % 300 == 0:
+        if batch % 100 == 0:
             torch.save(encoder.state_dict(), encoder_path)
             torch.save(decoder.state_dict(), decoder_path)
+            torch.save(discriminator.state_dict(), discriminator_path)
+            img = torch.clamp(y[0].detach(), -1, 1)
+
+            # Save Reconstructedd image
+            path = os.path.join(result_dir, f"{batch}_reconstructed.jpg")
+            img = Image.fromarray((img.cpu().numpy() * 127.5 + 127.5).astype(np.uint8).transpose(1,2,0), mode='RGB')
+            img.save(path)
+            
+            # Save Input image
+            img = torch.clamp(image[0].detach(), -1, 1)
+            path = os.path.join(result_dir, f"{batch}_input.jpg")
+            img = Image.fromarray((img.cpu().numpy() * 127.5 + 127.5).astype(np.uint8).transpose(1,2,0), mode='RGB')
+            img.save(path)
+
+
--- vae.py
+++ vae.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from modules import ChannelNorm, ReGLU
+from attention import WindowAttention
+from sinusoidal import PositionalEncoding2d
 
 class VAE(nn.Module):
     def __init__(self, encoder, decoder):
@@ -12,17 +14,17 @@ class VAE(nn.Module):
     def calclate_loss(self, x):
         mean, logvar = self.encoder(x)
         var = torch.exp(logvar)
-        loss_kl = (-1 - logvar + var + mean ** 2).mean()
-        z = torch.randn_like(mean) * torch.sqrt(var) + mean
+        loss_kl = (-1 - logvar + var).mean() + (mean ** 2).mean()
+        z = torch.randn_like(mean) * var + mean
         y = self.decoder(z)
-        loss_recon = (x-y).abs().mean()
-        return loss_recon, loss_kl
+        loss_recon = (x.detach()-y).abs().mean()
+        return loss_recon, loss_kl, y
     
     @torch.no_grad()
     def encode(self, x, sigma=0):
         mean, logvar = self.encoder(x)
         var = torch.exp(logvar)
-        z = torch.randn_like(mean) * torch.sqrt(var) * sigma + mean
+        z = torch.randn_like(mean) * var * sigma + mean
         return z
     
     @ torch.no_grad()
@@ -32,9 +34,9 @@ class VAE(nn.Module):
 class ResBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
-        self.dw_conv = nn.Conv2d(channels, channels, 7, 1, 3, groups=channels)
+        self.dw_conv = nn.Conv2d(channels, channels, 3, 1, 1, groups=channels//32)
         self.norm = ChannelNorm(channels)
-        self.ff = ReGLU(channels, ffn_mul=2)
+        self.ff = ReGLU(channels, ffn_mul=1)
 
     def forward(self, x):
         res = x
@@ -52,7 +54,7 @@ class ResStack(nn.Module):
         return self.seq(x)
 
 class Encoder(nn.Module):
-    def __init__(self, input_channels=3, latent_channels=16, channels=[32, 64, 128, 256], stages=[2, 2, 2, 2]):
+    def __init__(self, input_channels=3, latent_channels=4, channels=[64, 128, 256, 512], stages=[2, 2, 2, 2]):
         super().__init__()
         self.input_layer = nn.Conv2d(input_channels, channels[0], 1, 1, 0)
         self.output_layer = nn.Conv2d(channels[-1], latent_channels*2, 1, 1, 0)
@@ -75,28 +77,28 @@ class Encoder(nn.Module):
         return mean, logvar
 
 class Decoder(nn.Module):
-    def __init__(self, output_channels=3, latent_channels=16, channels=[512, 256, 128, 64], stages=[2, 2, 2, 2]):
+    def __init__(self, output_channels=3, latent_channels=4, channels=[512, 256, 128, 64], stages=[2, 2, 2, 2]):
         super().__init__()
         self.input_layer = nn.Conv2d(latent_channels, channels[0], 1, 1, 0)
         self.output_layer = nn.Conv2d(channels[-1], output_channels, 1, 1, 0)
         self.stages = nn.ModuleList([ResStack(c, l) for c, l in zip(channels, stages)])
         self.upsamples = nn.ModuleList([])
         for i, c in enumerate(channels):
-            if i == len(self.stages)-1:
+            if i == 0:
                 self.upsamples.append(nn.Identity())
             else:
-                self.upsamples.append(nn.ConvTranspose2d(c, channels[i+1], 2, 2, 0))
+                self.upsamples.append(nn.ConvTranspose2d(channels[i-1], c, 2, 2, 0))
 
     def forward(self, x):
         x = self.input_layer(x)
-        for a, b in zip(self.stages, self.upsamples):
+        for a, b in zip(self.upsamples, self.stages):
             x = a(x)
             x = b(x)
         x = self.output_layer(x)
         return x
 
 class Discriminator(nn.Module):
-    def __init__(self, input_channels=3, channels=[32, 64, 128, 256], stages=[2, 2, 2, 2], stem_size=2):
+    def __init__(self, input_channels=3, channels=[32, 64, 128, 256], stages=[2, 2, 2, 2], stem_size=1):
         super().__init__()
         self.input_layer = nn.Conv2d(input_channels, channels[0], stem_size, stem_size, 0)
         self.output_layer = nn.Conv2d(channels[-1], 1, 1, 1, 0)
@@ -116,16 +118,16 @@ class Discriminator(nn.Module):
         for a, b in zip(self.stages, self.downsamples):
             fake_x = a(fake_x)
             real_x = a(real_x)
-            fake_x = b(fake_x)
-            fake_x = b(fake_x)
             feat_loss += (fake_x-real_x).abs().mean()
+            fake_x = b(fake_x)
+            real_x = b(real_x)
         logit = self.output_layer(fake_x)
         return logit, feat_loss
 
     def calclate_logit(self, fake_x):
         fake_x = self.input_layer(fake_x)
         feat_loss = 0
-        for a, b in zip(self.tages, self.downsamples):
+        for a, b in zip(self.stages, self.downsamples):
             fake_x = a(fake_x)
             fake_x = b(fake_x)
         logit = self.output_layer(fake_x)
